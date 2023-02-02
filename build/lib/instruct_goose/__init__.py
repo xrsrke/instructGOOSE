81baf69f3bc4ccd128dd17b6ff4aa19aec81f12b
xrsrke <xariusdrake@hotmail.com>
2023-02-02 09:28:55 +0700

    clean

--- build/lib/instruct_goose/__init__.py
+++
@@ -1,6 +0,0 @@
-__version__ = "0.0.1"
-
-from instruct_goose.agent import Agent
-from instruct_goose.reward import RewardModel
-from instruct_goose.trainer import RLHFTrainer
-from instruct_goose.utils import create_reference_model, RLHFConfig
